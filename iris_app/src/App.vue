267ad31ccaf2b1ebad80901e36a6befed81002ed
Boadzie Daniel <boadziedaniel43@gmail.com>
2019-07-10 22:27:35 +0000

    Added Vue.js and Vuetify for the frontend

---
+++ iris_app/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-capitalized">
+        <span class="font-weight-light">Iris Flower Prediction</span>
+      </v-toolbar-title>
+    </v-toolbar>
+    <v-content>
+      <Home />
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import Home from "./components/Home";
+
+export default {
+  name: "App",
+  components: {
+    Home
+  },
+  data() {
+    return {
+      //
+    };
+  }
+};
+</script>
